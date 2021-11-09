0197c9d01818ca972fb10a6924d497e1948e3cb9
lucamagenes <lucamgns33@gmail.com>
2021-11-09 16:27:37 +0100

    modificato html v-for aggiunto v-if modificato js users.array aggiunto ciclo for js

--- app.js
+++ app.js
@@ -1,58 +1,20 @@
 const root = new Vue({
     el: '#root',
     data: {
-        users: [
-            {
-                user: 1,
-                userMail: '',
-            },
-            {
-                user: 2,
-                userMail: '',
-            },
-            {
-                user: 3,
-                userMail: '',
-            },
-            {
-                user: 4,
-                userMail: '',
-            },
-            {
-                user: 5,
-                userMail: '',
-            },
-            {
-                user: 6,
-                userMail: '',
-            },
-            {
-                user: 7,
-                userMail: '',
-            },
-            {
-                user: 8,
-                userMail: '',
-            },
-            {
-                user: 9,
-                userMail: '',
-            },
-            {
-                user: 10,
-                userMail: '',
-            },
-        ],
+        users: [],
     },
     mounted() {
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(resp => {
-                console.log(resp.data.response);
-                const mail = resp.data.response;
-                this.users.userMail = mail;
-                console.log(this.users.userMail);
-            })
+
+        for (let i = 0; i < 10; i++) {
+
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(resp => {
+                    console.log(resp.data.response);
+                    const mail = resp.data.response;
+                    this.users.push(mail)
+                })
+        };
 
     }
 })
