845957e94c2d95a8dc3e896f5feeda1dc94254eb
lucamagenes <lucamgns33@gmail.com>
2021-11-09 15:57:07 +0100

    aggiunto axios in mounted

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 const root = new Vue({
     el: '#root',
     data: {
-        userNumber: '',
-        pcNumber: '',
+        users: [
+            {
+                user: 1,
+                userMail: '',
+            },
+            {
+                user: 2,
+                userMail: '',
+            },
+            {
+                user: 3,
+                userMail: '',
+            },
+            {
+                user: 4,
+                userMail: '',
+            },
+            {
+                user: 5,
+                userMail: '',
+            },
+            {
+                user: 6,
+                userMail: '',
+            },
+            {
+                user: 7,
+                userMail: '',
+            },
+            {
+                user: 8,
+                userMail: '',
+            },
+            {
+                user: 9,
+                userMail: '',
+            },
+            {
+                user: 10,
+                userMail: '',
+            },
+        ],
     },
     mounted() {
         axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail');
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(resp => {
+                console.log(resp.data.response);
+                const mail = resp.data.response;
+                this.users.userMail = mail;
+                console.log(this.users.userMail);
+            })
 
     }
 })
